3e3c1bf65723db570501168d0251ee8faa092c11
SavitskiDenis <random.ssd@yandex.by>
2021-11-12 19:34:39 +0300

    feat: implement CipheringTransform

--- ciphering_cli/transforms/ciphering_transform.js
+++ ciphering_cli/transforms/ciphering_transform.js
@@ -1,8 +1,30 @@
 const { Transform } = require('stream');
 
 class CipheringTransform extends Transform {
+    constructor (config) {
+        super();
+        this.config = config;
+    }
+
+    transformChunk (chunk) {
+        const str = chunk.toString();
+        let res = '';
+        let code;
+        for (const c of str) {
+            code = c.charCodeAt(0);
+            if (code >= 65 && code <= 90) {
+                res += this.config.convertUpperCode(code);
+            }   else if (code >= 97 && code <= 122) {
+                res += this.config.convertLowerCode(code);
+            }   else {
+                res += c;
+            }
+        }
+        return res;
+    }
+
     _transform (chunk, _, cb) {
-        this.push(chunk);
+        this.push(this.transformChunk(chunk));
         cb();
     }
 }
